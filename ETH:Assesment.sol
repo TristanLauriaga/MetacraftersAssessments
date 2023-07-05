fdf8268c79d47d551e49dd2924f6693ef4a2ef24
TristanLauriaga <137780534+TristanLauriaga@users.noreply.github.com>
2023-07-05 05:06:23 -0700

    ETH:Assesment.sol

--- ETH:Assesment.sol
+++ ETH:Assesment.sol
@@ -3,21 +3,17 @@ pragma solidity 0.8.18;
 
 contract MyToken {
 
-    //declaring public variables
     string public tokenName = "NOBEL";
     string public tokenAbrv = "NBL";
     uint public totalSupply = 0;
     
-    //mapping of the address
     mapping(address => uint) public balances;
 
-    //declaring the mint function
     function mint(address _add, uint _val) public  {
         totalSupply += _val;
         balances[_add] += _val;
     }
 
-    //declaring the burn function
     function burn(address _add, uint _val) public  {
         if (balances[_add] >= _val ){
             totalSupply -= _val;
